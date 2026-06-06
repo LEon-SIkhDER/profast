d3526cfc972252a6afcd1416cbb779cee8e5249d
LEon-SIkhDER <leondesktop7580@gmail.com>
2026-06-06 20:46:10 +0600

    about us section import problem solve

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -31,7 +31,7 @@ import PendingDeliveries from "../Pages/Dashboard/PendingDeliveries/PendingDeliv
 import RiderRoute from "../PrivateRoute/RiderRoute";
 import CompletedDeliveries from "../Pages/Dashboard/CompletedDeliveries/CompletedDeliveries";
 import ParcelDetails from "../Pages/Dashboard/ParcelDetails/ParcelDetails";
-import AboutUsPage2 from "../Pages/AboutUs/AboutUsPage2";
+// import AboutUsPage2 from "../Pages/AboutUs/AboutUsPage2";
 
 
 
